8b2ef174366dd5272d0a8f30b0a100568b78d399
爆頭專家 <tom19960222@gmail.com>
2018-03-04 09:55:12 +0800

    Send message to Slack

--- runtest.sh
+++ runtest.sh
@@ -7,6 +7,8 @@ T_DIR=${JMETER_HOME}/testfiles/MPG-Backend
 RESULT_BUCKET=mpg-test-result
 DATETIME=`date '+%Y-%m-%d_%H-%M-%S'`
 TEST_FILE_URL=https://s3-ap-northeast-1.amazonaws.com/mpg-test-result/Backend_Test.jmx
+RESULT_URL=https://s3-ap-northeast-1.amazonaws.com/mpg-test-result/Backend_API_Test/${DATETIME}/index.html
+PAYLOAD={\"CodePipeline.job\":{\"data\":{\"actionConfiguration\":{\"configuration\":{\"UserParameters\":\"See test report at ${RESULT_URL}\"}}}}}
 
 # Reporting dir: start fresh
 R_DIR=${T_DIR}/report
@@ -29,3 +31,4 @@ cat ${T_DIR}/Backend_Test.jtl
 echo "==== HTML Test Report ===="
 echo "See HTML test report in ${R_DIR}/index.html"
 aws s3 cp --recursive ${R_DIR} s3://${RESULT_BUCKET}/Backend_API_Test/${DATETIME}
+aws lambda invoke --invocation-type Event --function-name CodePipelineNotification --region ap-northeast-1 --log-type None --payload ${PAYLOAD} 
