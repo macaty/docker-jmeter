58fb59620b0ba8ba71687f986d1d58de268ddd53
爆頭專家 <tom19960222@gmail.com>
2018-03-04 10:20:30 +0800

    Fix wrong payload

--- build_payload.py
+++ build_payload.py
@@ -4,4 +4,4 @@ import sys
 data = json.loads("""{"CodePipeline.job":{"data":{"actionConfiguration":{"configuration":{"UserParameters":"GGININ"}}}}}""")
 data['CodePipeline.job']['data']['actionConfiguration']['configuration']['UserParameters'] = sys.stdin.read()
 
-print(data)
+print(json.dumps(data))
